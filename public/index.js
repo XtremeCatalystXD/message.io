01891a62cbe42933eed61a8fc75fb9dd09211d04
Kyle <kylzac717@hotmail.com>
2018-10-24 21:21:30 +1100

    Design changes

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 var socket = io();
 var username;
-document.getElementById('chat').style.display = 'none';
+//document.getElementById('chat').style.display = 'none';
 
 function setUsername() {
     username = document.getElementById('username').value;
@@ -10,12 +10,15 @@ function setUsername() {
 function sendMessage() {
     var message = document.getElementById('message').value;
     socket.emit('send-message', message);
+    document.getElementById('message').value = "";
 }
 
 socket.on('valid-username', function() {
     document.getElementById('userForm').style.display = 'none';
-    document.getElementById('chat').style.display = '';
-    document.getElementById('header').innerHTML = "Welcome, " + username + "!";
+    //document.getElementById('chat').style.display = '';
+    document.getElementById('userFormMessage').style.display = 'none';
+    document.getElementsByTagName('header')[0].innerHTML = "Welcome, <b>" + username + "</b>!";
+    document.getElementsByTagName('footer')[0].innerHTML = "<form id='messageForm' onsubmit='sendMessage(); return false'><label for='message'>Enter a message: </label><input type='text' id='message'><input type='submit' value='Send'></form>";
 });
 
 socket.on('invalid-username', function(data) {
@@ -24,6 +27,6 @@ socket.on('invalid-username', function(data) {
 
 socket.on('newMessage', function(msg, userId) {
     var chat = document.getElementById('chatMessages');
-    chat.innerHTML += userId + ": " + msg + "<br>";
+    chat.innerHTML += "<b>" + userId + ":</b> " + msg + "<br>";
     chat.scrollTop = chat.scrollHeight;
 });
