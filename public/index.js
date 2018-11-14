898ca423b912f9b60629d6e80abaeb5107a14306
Zucchini <kylzac717@hotmail.com>
2018-11-14 21:50:46 +1100

    Added htmlspecialchars function for js

--- public/index.js
+++ public/index.js
@@ -11,6 +11,17 @@ function login() {
     socket.emit('login', username, password);
 }
 
+function htmlspecialchars(str) {
+    if (typeof(str) == "string") {
+        str = str.replace(/&/g, "&amp;"); /* must do &amp; first */
+        str = str.replace(/"/g, "&quot;");
+        str = str.replace(/'/g, "&#039;");
+        str = str.replace(/</g, "&lt;");
+        str = str.replace(/>/g, "&gt;");
+    }
+    return str;
+}
+
 function sendMessage() {
     var message = document.getElementById('message');
 
