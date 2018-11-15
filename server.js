ab81bfd2661f75d4f12e8b379701a53a3ff73863
Zucchini <kylzac717@hotmail.com>
2018-11-15 19:46:30 +1100

    Reinstated SQL
    The problem was configuration on the SQL Server

--- server.js
+++ server.js
@@ -5,17 +5,20 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var users = [];
-// var Connection = require('tedious').Connection;
-// var config = {
-//     userName: 'messageio',  
-//     password: 'password',  
-//     // server: <ip address>
-// };
-//var Request = require('tedious').Request;
-//var connection = new Connection(config);
-// connection.on('connect', function(err) {
-//     console.log(err);
-// });
+var Connection = require('tedious').Connection;
+var config = {
+    userName: 'messageio',  
+    password: 'password',  
+    // server: <ip address>,
+    options:{
+        database: "messageio"
+    }
+};
+var Request = require('tedious').Request;
+var connection = new Connection(config);
+connection.on('connect', function(err) {
+    console.log(err);
+});
 
 http.listen(port, function() {
     console.log("Node socket server has started successfully.");
@@ -27,19 +30,19 @@ io.on('connection', function(socket) {
     console.log(socket.id);
 
     socket.on('login', function(username, password) {
-        // var loginTestStatement = "SELECT UserExtendedInfo.UserId FROM UserExtendedInfo INNER JOIN Users ON UserExtendedInfo.UserId = Users.UserId WHERE Users.Username = '" + username + "' AND UserExtendedInfo.Password = HASHBYTES('SHA2_256','" + password +  "');";
+        var loginTestStatement = "SELECT UserExtendedInfo.UserId FROM UserExtendedInfo INNER JOIN Users ON UserExtendedInfo.UserId = Users.UserId WHERE Users.Username = '" + username + "' AND UserExtendedInfo.Password = HASHBYTES('SHA2_256','" + password +  "');";
 
-        // request = new Request(loginTestStatement, function(err) {  
-        //     if (err) {
-        //         console.log(err);
-        //     }
-        // });
+        request = new Request(loginTestStatement, function(err) {  
+            if (err) {
+                console.log(err);
+            }
+        });
         
-        // request.on('row', function(columns) {
-        //         console.log(column.metadata.colName.UserId.value);
-        // });
+        request.on('row', function(columns) {
+                console.log(column.metadata.colName.UserId.value);
+        });
 
-        // connection.execSql(request);
+        connection.execSql(request);
         
         if (username != "") {
             socket.username = username;
