b15859e509df68d6a480da8a76fae97e63ca05f6
~kbzz <20939953+zucchinii@users.noreply.github.com>
2020-07-20 00:13:21 +1000

    Revert "Revert "Create User""
    This reverts commit 676cb2da790ce8836bac50fca97951f6895a476d.

--- server.js
+++ server.js
@@ -29,11 +29,36 @@ app.use(express.static('public'));
 io.on('connection', function(socket) {
     console.log(socket.id);
 
+    socket.on('createUser', function(username, password, email, name, age) {
+
+        var testEmailStatement = "SELECT * FROM UserId WHERE Users.Email = '" + email + "';";
+        testEmailRequest = new Request(testEmailStatement, function(err, rowCount) {  
+            if (err) {
+                console.log(err);
+            } else {
+                if (rowCount == 0) {
+                    var userCreateStatement = "INSERT INTO dbo.Users VALUES ('" + username + "', 1, '" + email + "')";
+                    createUserRequest = new Request(userCreateStatement, function(err) {  
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            var userExtendedInfoStatement = "INTO dbo.UserExtendedInfo VALUES (" + databaseUserId + ", HASHBYTES('SHA2_256','" + password + "'), '" + email + "', " + name + "," + age + ", null, null)";
+                        }
+                    });
+
+                    connection.execSql(createUserRequest);
+        	    }
+            }
+        });
+
+        connection.execSql(testEmailRequest);
+    });
+
     socket.on('login', function(username, password) {
         var userId = '';
         var loginTestStatement = "SELECT UserExtendedInfo.UserId FROM UserExtendedInfo INNER JOIN Users ON UserExtendedInfo.UserId = Users.UserId WHERE Users.Username = '" + username + "' AND UserExtendedInfo.Password = HASHBYTES('SHA2_256','" + password +  "');";
 
-        request = new Request(loginTestStatement, function(err, rowCount, rows) {  
+        loginRequest = new Request(loginTestStatement, function(err, rowCount, rows) {  
             if (err) {
                 console.log(err);
             } else {
@@ -49,13 +74,13 @@ io.on('connection', function(socket) {
             }
         });
         
-        request.on('row', function(columns) {
+        loginRequest.on('row', function(columns) {
             columns.forEach(column => {
                 userId = column.value;
             });
         });
 
-        connection.execSql(request);
+        connection.execSql(loginRequest);
     });
 
     socket.on('send-message', function(msgData) {
